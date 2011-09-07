4473f6079cfc673ae4b912b1b2e9b63156a8e5d7
travis134 <tsein@asu.edu>
2011-09-07 16:25:55 -0700

    Added seekbars for radius delay and etc in android client

--- client/android/Whats_Happening_At_ASU/src/com/asusoda/whatshappeningatasu/Whats_Happening_At_ASUActivity.java
+++ client/android/Whats_Happening_At_ASU/src/com/asusoda/whatshappeningatasu/Whats_Happening_At_ASUActivity.java
@@ -22,34 +22,43 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-public class Whats_Happening_At_ASUActivity extends Activity implements OnClickListener {
+public class Whats_Happening_At_ASUActivity extends Activity implements OnClickListener, OnSeekBarChangeListener {
 	private final String TAG = "Whats Happening @ ASU";
 	private android.location.Location currentLocation;
-	Button buttonStart, buttonStop;
+	Button buttonSearch;
+	SeekBar radiusBar, delayBar;
+	private int radius = 125;
+	private int delay = 1440;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
+    	Log.i(TAG, "Opened the app");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        buttonStart = (Button)findViewById(R.id.buttonStart);
-        buttonStop = (Button)findViewById(R.id.buttonStop);
+        buttonSearch = (Button)findViewById(R.id.buttonSearch);
+        radiusBar = (SeekBar)findViewById(R.id.radiusBar);
+        delayBar = (SeekBar)findViewById(R.id.delayBar);
         
-        buttonStart.setOnClickListener(this);
-        buttonStop.setOnClickListener(this);
+        buttonSearch.setOnClickListener(this);
+        radiusBar.setOnSeekBarChangeListener(this);
+        delayBar.setOnSeekBarChangeListener(this);
         
-        
+        Log.i(TAG, "Creating location manager...");
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         
+        Log.i(TAG, "Creating location listener...");
         LocationListener locationListener = new LocationListener() {
         	
         	public void onLocationChanged(android.location.Location location) {
-              // Called when a new location is found by the network location provider.
               locationChanged(location);
             }
 
@@ -60,11 +69,13 @@ public class Whats_Happening_At_ASUActivity extends Activity implements OnClickL
             public void onProviderDisabled(String provider) {}
           };
           
+          Log.i(TAG, "Registering location manager with location listener...");
           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
     }
     
     private void locationChanged(android.location.Location location) {
+    	Log.i(TAG, "Location changed: (" + location.getLatitude() + ", " + location.getLongitude() + ")");
     	currentLocation = location;
     }
     
@@ -72,19 +83,19 @@ public class Whats_Happening_At_ASUActivity extends Activity implements OnClickL
 	@Override
 	public void onClick(View v) {
 	    switch (v.getId()) {
-	    case R.id.buttonStart:
-	      Log.d(TAG, "onClick: starting");
-	      Toast.makeText(this, "Parsing events...", Toast.LENGTH_SHORT).show();
+	    case R.id.buttonSearch:
 	      if(currentLocation == null) {
+	    	  Toast.makeText(this, "No location found, make sure GPS is turned on.", Toast.LENGTH_LONG).show();
 	    	 break;
 	      }
-		  for(Event ev : runJSONParser(currentLocation.getLatitude(), currentLocation.getLongitude()))
+	      Log.i(TAG, "onClick: Searching for events within a " + radius + "m radius of (" + currentLocation.getLatitude() + ", " + currentLocation.getLongitude() + ")");
+	      Toast.makeText(this, "Searching for events within a " + radius + "m radius...", Toast.LENGTH_LONG).show();
+		  List<Event> events = runJSONParser(currentLocation.getLatitude(), currentLocation.getLongitude());
+	      for(Event ev : events)
 		  {
-			  Log.d(TAG, ev.getName() + " - " + ev.getOrganization().getOwner().getUsername());
+			  Log.i(TAG, ev.getName() + " in " + ev.getLocation().getBuilding().getName());
 		  }
-	      break;
-	    case R.id.buttonStop:
-	      Log.d(TAG, "onClick: stopping");
+		  Toast.makeText(this, "Found " + events.size() + " events...", Toast.LENGTH_LONG).show();
 	      break;
 	    }
 	  }
@@ -113,7 +124,7 @@ public class Whats_Happening_At_ASUActivity extends Activity implements OnClickL
 	public List<Event> runJSONParser(double latitude, double longitude) {
 		List<Event> events = new ArrayList<Event>();
         try{
-        	InputStream source = getJSONData("http://slyduck.com/api/events/near/" + latitude + "," + longitude + "/?radius=200");
+        	InputStream source = getJSONData("http://slyduck.com/api/events/near/" + latitude + "," + longitude + "/?radius=" + radius + "&delay=" + delay);
         	Gson gson = new Gson();
         	Reader reader = new InputStreamReader(source);
         	events = gson.fromJson(reader, new TypeToken<ArrayList<Event>>(){}.getType());
@@ -122,4 +133,29 @@ public class Whats_Happening_At_ASUActivity extends Activity implements OnClickL
         }
         return events;
     }
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		switch (seekBar.getId()) {
+		case R.id.delayBar:
+			delay = progress;
+			break;
+		case R.id.radiusBar:
+			radius = progress;
+			break;
+		}
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
 }
